671e1c6443b3be855f9aae5032a06eed727eab8d
barshiro <barshiro2@gmail.com>
2025-06-18 03:00:43 +0500

    Update redis.adapter.ts

--- src/websocket/redis.adapter.ts
+++ src/websocket/redis.adapter.ts
@@ -4,21 +4,19 @@ import { createAdapter } from '@socket.io/redis-adapter';
 import { createClient } from 'redis';
 
 export class RedisIoAdapter extends IoAdapter {
-  private redisAdapter: any;
+  private adapterConstructor: ReturnType<typeof createAdapter>;
 
-  constructor(app: any) {
-    super(app);
-    const pubClient = createClient({ url: 'redis://localhost:6379' });
+  async connectToRedis(): Promise<void> {
+    const pubClient = createClient({ url: 'redis://default:xNukaJlNeUllBgKlHtyzBRMPCApKeBzy@turntable.proxy.rlwy.net:41633' });
     const subClient = pubClient.duplicate();
-    Promise.all([pubClient.connect(), subClient.connect()]).catch(err => {
-      console.error('Redis connection error:', err);
-    });
-    this.redisAdapter = createAdapter(pubClient, subClient);
+
+    await Promise.all([pubClient.connect(), subClient.connect()]);
+    this.adapterConstructor = createAdapter(pubClient, subClient);
   }
 
   createIOServer(port: number, options?: ServerOptions): any {
     const server = super.createIOServer(port, options);
-    server.adapter(this.redisAdapter);
+    server.adapter(this.adapterConstructor);
     return server;
   }
 }
