b62ba33ec3f4f0b63bd6337fa97d609d1851adc1
barshiro <barshiro2@gmail.com>
2025-06-17 23:29:06 +0500

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,17 +3,16 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UserSchema } from '../models/user.schema';
+import { GroupSchema } from '../models/group.schema'; // Импорт схемы группы
+import { GroupMembersSchema } from '../models/group-members.schema'; // Импорт схемы участников группы
 import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
   imports: [
-     MongooseModule.forFeature([
-      { name: 'Group', schema: GroupSchema },
-      { name: 'GroupMembers', schema: GroupMembersSchema },
-      { name: 'Roles', schema: RolesSchema },
-      { name: 'KeysStorage', schema: KeysStorageSchema },
+    MongooseModule.forFeature([
       { name: 'User', schema: UserSchema },
-      { name: 'Notification', schema: NotificationSchema }, // Добавляем Notification
+      { name: 'Group', schema: GroupSchema }, // Добавляем модель Group
+      { name: 'GroupMembers', schema: GroupMembersSchema } // Добавляем модель GroupMembers
     ]),
     NotificationsModule,
   ],
